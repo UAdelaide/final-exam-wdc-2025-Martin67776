c2a37c43204fdef96e3fbec68989b374de7b6f1b
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 02:31:42 +0000

    [automatic commit] 2025-06-20 02:31:42 UTC 180.150.112.176

--- part1/task1.5.sql
+++ part1/task1.5.sql
@@ -4,7 +4,7 @@ INSERT INTO Users (username, email, password_hash, role) VALUES ('carol123', 'ca
 INSERT INTO Users (username, email, password_hash, role) VALUES ('merc123', 'merc@example.com', 'hashed012', 'walker');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('user123', 'user@example.com', 'hashed345', 'owner');
 INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Max', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
-INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Bella', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
-INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Max', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
-INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Max', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
-INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Max', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
+INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Bella', 'small') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
+INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Sheep', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
+INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Cat', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
+INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Duck', 'medium') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'alice123');
