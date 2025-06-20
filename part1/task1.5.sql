b3fc279b35d157634465169603f4f3cef29fcb98
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 02:27:00 +0000

    [automatic commit] 2025-06-20 02:27:00 UTC 180.150.112.176

--- part1/task1.5.sql
+++ part1/task1.5.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
-INSERT INTO users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
+INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
+INSERT INTO Users (username, email, password_hash, role) VALUES ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
 INSERT INTO users (username, email, password_hash, role) VALUES ('carol123', 'carol@example.com', 'hashed789', 'owner');
 INSERT INTO users (username, email, password_hash, role) VALUES ('merc123', 'merc@example.com', 'hashed012', 'walker');
 INSERT INTO users (username, email, password_hash, role) VALUES ('user123', 'user@example.com', 'hashed345', 'owner');
-INSERT INTO 
+INSERT INTO Dogs ()
