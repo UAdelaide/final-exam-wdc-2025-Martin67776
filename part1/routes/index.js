bae0760cf5e9d3ea6115099affbaa0d636f413ed
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 03:06:12 +0000

    [automatic commit] 2025-06-20 03:06:12 UTC 180.150.112.176

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -103,6 +103,11 @@ let db;
     const [rows2] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (rows2[0].count === 0) {
       await db.execute(`
+        INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium');
+        INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small');
+        INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Sheep', 'large');
+        INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'user123'), 'Cat', 'medium');
+        INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Duck', 'small');
       `);
     }
   } catch (err) {
