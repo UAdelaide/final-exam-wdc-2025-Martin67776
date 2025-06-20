8974082c0dac0a51f4f7ff7b01a8076a921d7377
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 03:55:33 +0000

    [automatic commit] 2025-06-20 03:55:33 UTC 180.150.112.176

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -138,7 +138,7 @@ router.get('/api/dogs', async (req, res) => {
 
 router.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [open] = await db.execute('SELECT WalkRequests.request_id AS request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM Users INNER JOIN Dogs ON Users.user_id = Dogs.owner_id INNER JOIN WalkRequests ON Dogs.dog_id = WalkRequests.dog_id WHERE WalkRequests.status = 'open';`);
+    const [open] = await db.execute(`SELECT WalkRequests.request_id AS request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM Users INNER JOIN Dogs ON Users.user_id = Dogs.owner_id INNER JOIN WalkRequests ON Dogs.dog_id = WalkRequests.dog_id WHERE WalkRequests.status = 'open';`);
     return res.json(open);
   } catch (dErr) {
     return res.status(500).json({ error: 'DB error' });
