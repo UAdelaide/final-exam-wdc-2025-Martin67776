7c028a7a2ad2de43269eaf66e2fbed2b44f80fe6
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 02:37:23 +0000

    [automatic commit] 2025-06-20 02:37:23 UTC 180.150.112.176

--- part1/task1.5.sql
+++ part1/task1.5.sql
@@ -10,6 +10,6 @@ INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Cat', 'medium') WHERE ow
 INSERT INTO Dogs (owner_id, name, size) VALUES (owner, 'Duck', 'small') WHERE owner IN (SELECT user_id FROM Users WHERE username = 'carol123');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 08:00:00', 30, 'Parklands', 'open') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Max');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 09:30:00', 30, 'Beachside Ave', 'accepted') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Bella');
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 09:00:00', 30, 'Parklands', 'open') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Sheep');
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 09:00:00', 30, 'Parklands', 'completed') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Sheep');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 10:00:00', 30, 'Parklands', 'open') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Cat');
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 11:00:00', 30, 'Parklands', 'open') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Duck');
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES (dog, '2025-06-10 11:00:00', 30, 'Parklands', 'cancelled') WHERE dog IN (SELECT dog_id FROM Dogs WHERE name = 'Duck');
