84c1e1cd831f91a56992476ffac44dee4c044ec8
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 04:57:42 +0000

    [automatic commit] 2025-06-20 04:57:42 UTC 180.150.112.176

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -16,7 +16,7 @@ const DogPic = {
     },
     methods: {
         fetchImage(count) {
-            if (count === 10) {
+            if (count === 9) {
                 fetch('https://dog.ceo/api/breeds/image/random', {
                     method: 'GET'
                 }).then((res) => res.json())
@@ -25,7 +25,7 @@ const DogPic = {
                 }).catch((err) => {
                     throw err;
                 });
-                c
+                counter = 0;
             }
         }
     }
