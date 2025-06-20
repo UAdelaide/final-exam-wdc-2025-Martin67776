31617d50287e02c2a12a77e36d2a6fdc61841b5b
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 04:05:36 +0000

    [automatic commit] 2025-06-20 04:05:36 UTC 180.150.112.176

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -144,7 +144,7 @@ router.get('/api/walkrequests/open', async (req, res) => {
 
 router.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [walker] = await db.execute('SELECT Users.username AS walker_username, COUNT(WalkRating.walker_id)');
+    const [walker] = await db.execute('SELECT Users.username AS walker_username, COUNT(WalkRating.walker_id) AS total_rating, ');
     return res.json(walker);
   } catch (dErr) {
     return res.status(500).json({ error: 'DB error' });
