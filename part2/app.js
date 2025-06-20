0ca6505779e7b5339c8418965891a5ebfc76ac7a
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 06:24:35 +0000

    [automatic commit] 2025-06-20 06:24:35 UTC 180.150.112.176

--- part2/app.js
+++ part2/app.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
