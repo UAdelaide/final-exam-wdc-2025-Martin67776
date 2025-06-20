5742cd5b00c61026e20f68fe6a6b5589f33e3973
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 04:58:43 +0000

    [automatic commit] 2025-06-20 04:58:43 UTC 180.150.112.176

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -1,7 +1,7 @@
 const DogPic = {
     template: `
     <div>
-        <button type="button" @click="fetchImage(counter++)">Click 10 times to see the picture</button>
+        <button type="button" @click="fetchImage(counter++)">Click 10 times to see a new picture</button>
         <img :src="picLink">
     </div>
     `,
