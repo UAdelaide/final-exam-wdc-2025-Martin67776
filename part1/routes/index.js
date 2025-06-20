23053735bafc41981ba23ad3b844248c5abbe5c0
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 03:38:48 +0000

    [automatic commit] 2025-06-20 03:38:48 UTC 180.150.112.176

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -129,10 +129,12 @@ let db;
 router.get('/api/dogs', async (req, res) => {
   try {
     const [dogs] = await db.execute('SELECT Dogs.name AS dog_name, Dogs.size AS size, Users.username AS owner_username FROM Users INNER JOIN Dogs ON Users.user_id = Dogs.owner_id');
-    res.json(dogs);
+    return res.json(dogs);
   } catch (dErr) {
-    return res.status(500).json({error: 'DB error'});
+    return res.status(500).json({ error: 'DB error' });
   }
 });
+
+router.get('')
 
 module.exports = router;
