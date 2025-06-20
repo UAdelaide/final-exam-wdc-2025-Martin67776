1e5477b844aa3737a76196b24b49e63885991245
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-20 03:41:08 +0000

    [automatic commit] 2025-06-20 03:41:08 UTC 180.150.112.176

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -137,7 +137,7 @@ router.get('/api/dogs', async (req, res) => {
 
 router.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [open] = await db.execute();
+    const [open] = await db.execute('SELECT request_id, ');
   } catch (dErr) {
     return res.status(500).json({ error: 'DB error' });
   }
