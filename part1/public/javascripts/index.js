fb0309b995f869fa5268a7220fe97cc167c6dc03
Zhelian Xiong <a1899964@adelaide.edu.au>
2025-06-21 05:35:48 +0000

    [automatic commit] 2025-06-21 05:35:48 UTC 180.150.112.176

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -13,7 +13,7 @@ Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapi
 
 Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.</p>
         <div class="picture">
-            <button type="button" @click="fetchImage(counter++)">Click 10 times to see a new picture</button>
+            <button class="spin-button" type="button" @click="fetchImage(counter++)">Click 10 times to see a new picture</button>
             <img :src="picLink">
         </div>
     </div>
